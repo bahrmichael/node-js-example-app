f16098c0364a820914ec2be801b3438383adb430
Michael Bahr <mbahr92@gmail.com>
2018-09-07 18:45:39 +0200

    added coverage reporting for jest

---
+++ src/test/jest.conf.js
@@ -0,0 +1,18 @@
+module.exports = {
+	coverageDirectory: '<rootDir>/target/test-results/',
+	collectCoverageFrom: [
+		"src/main/**/*.js"
+	],
+	globals: {
+		__TRANSFORM_HTML__: true
+	},
+	// moduleNameMapper: {
+	// 	'app/(.*)': '<rootDir>/src/main/$1'
+	// },
+	reporters: [
+		'default'
+	],
+	testMatch: [ '<rootDir>/src/test/**/+(*.)+(spec.js)' ],
+	rootDir: '../../',
+	testURL: "http://localhost/"
+};
