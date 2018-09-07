2af6c8b19210749847d7129a838036737251664f
Jens <jens@dieburkards.de>
2018-09-07 17:21:05 +0200

    Added supertest for http testing

--- app.test.js
+++ app.test.js
@@ -1,4 +1,5 @@
 const app = require('./app.js');
+let request = require('supertest');
 
 // Just a simple initial unit test
 test('Adds 1 to the counter and removes it again', () => {
@@ -7,4 +8,12 @@ test('Adds 1 to the counter and removes it again', () => {
 	expect(app.counter.toBe(initialValue + 1));
 	app.removeOne();
 	expect(app.counter.toBe(initialValue));
+});
+
+request = request('http://localhost:5555');
+
+test('Tests the availability of the local server', () => {
+	request.get('/').expect(200, function(err) {
+		console.log(err);
+	});
 });
