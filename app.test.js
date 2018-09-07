81b88e6dc5515c79a0bfff227f57d0e204a88b71
Jens <jens@dieburkards.de>
2018-09-07 17:39:10 +0200

    Added some tests for the server

--- app.test.js
+++ app.test.js
@@ -1,15 +1,15 @@
 //const app = require('./app.js');
-//let request = require('supertest');
+let request = require('supertest');
 
 // Just a simple initial unit test
 test('Emptyy test', () => {
 	expect(true).toBe(true);
 });
 
-//request = request('http://localhost:5555');
+request = request('http://localhost:5555');
 
-// test('Tests the availability of the local server', () => {
-// 	request.get('/').expect(200, function(err) {
-// 		console.log(err);
-// 	});
-// });
+test('Tests the availability of the local server', () => {
+	request.get('/').expect(200, function(err) {
+		console.log(err);
+	});
+});
