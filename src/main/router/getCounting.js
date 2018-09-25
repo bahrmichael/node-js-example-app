fd161e0a786a76e435e5bedb59de3d7617ebd4c8
Jens <jens@dieburkards.de>
2018-09-25 14:23:47 +0200

    Fixed logic error in getCounting.js so the counter wont' always restart

--- src/main/router/getCounting.js
+++ src/main/router/getCounting.js
@@ -1,7 +1,7 @@
-let getCounting = function(req, res) {
-	let Counter = require('../logic/counter');
-	let counter = new Counter(0);
+let Counter = require('../logic/counter');
+let counter = new Counter(0);
 
+let getCounting = function(req, res) {
 	res.json({ counterValue: counter.returnCurrentValueAndIncrementIt() });
 };
 
