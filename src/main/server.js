0a4bcc56e2a73fba301263b526776e4936f1f53b
Jens <jens@dieburkards.de>
2018-09-26 15:58:30 +0200

    Refactored server and implemented unit tests.

--- src/main/server.js
+++ src/main/server.js
@@ -1,18 +1,40 @@
 let express = require('express');
-let app = express();
 
-let api = require('./router/api');
+class Server {
+	constructor() {
+		this.app = express();
+	}
+	startServer(portNumber) {
+		if (typeof portNumber === 'number' && portNumber >= 0 && portNumber <= 65525) {
+		// if port = 0  or if the port is ommitted it will automatically select a free port
+			this.port = process.env.PORT || portNumber;
+			this.server = this.app.listen(this.port, function() {
+				this.port = this.address().port;
+				console.log('Express server listening on port ' + this.port + '.');
+			});
+		} else {
+			throw new Error('invalid portNumber');
+		}
+	}
+	routeAPI() {
+		if (typeof this.port === 'number' && this.port >= 0 && this.port <= 65525) {
+			let api = require('./router/api');
+			//call and mount the api to /api
+			this.app.use('/api', api);
+		} else {
+			throw new Error('startServer not called');
+		}
+	}
+	closeServer() {
+		this.server.close();
+		console.log('Express server closed. Port ' + this.port + ' now unavaillable.');
+	}
+}
 
-//call and mount the api to /api
-app.use('/api', api);
+// Uncomment to spawn a server:
 
-// if port = 0  or if the port is ommitted it will automatically select a free port
-let port = process.env.PORT || 1337;
+// let server = new Server();
+// server.startServer(1337);
+// server.routeAPI();
 
-let server = app.listen(port, function() {
-	port = this.address().port;
-	console.log('Express server listening on port ' + port);
-});
-
-module.exports = server;
-module.exports = app;
+module.exports = Server;
--- src/test/server.spec.js
+++ src/test/server.spec.js
@@ -1,24 +1,59 @@
+let Server = require('../main/server');
+let server = new Server();
+
 let request = require('supertest');
+//request = request('http://localhost:1337');
 
-request = request('http://localhost:1337');
-
-describe('Test GET /api/counter for correct counting', () => {
-	it('Responds with JSON', function(done) {
-		request
-			.get('/api/counter')
-			.set('Accept', 'text/plain')
-			.expect('Content-Type', /json/)
-			.expect(200, done);
+describe('Test server features', () => {
+	it('Doesnt initiate Server incorrectly', () => {
+		expect(() => {
+			server.startServer(-1337);
+		}).toThrow();
+		expect(() => {
+			server.startServer('fuck');
+		}).toThrow();
+		expect(() => {
+			server.routeAPI();
+		}).toThrow();
+		expect(server.port).toBe(undefined);
 	});
 
-	it('Counts in +1 increments', function(done) {
-		request.get('/api/counter', function(res) {
-			let testingValue = res.body.counterValue;
-			request.get('/api')
-				.expect({ counterValue: testingValue + 1 }, done);
-		}).end(function(err, res) {
-			if (err) return done(err);
-			done();
+	it('Does initate Server correctly', () => {
+		server.startServer(1337);
+		server.routeAPI();
+		expect(server.port).toEqual(1337);
+	});
+
+	describe('Mount /api/counter correctly', () => {
+		it('and responds with JSON', function(done) {
+			request(server.app)
+				.get('/api/counter')
+				.set('Accept', 'text/plain')
+				.expect('Content-Type', /json/)
+				.expect(200, done);
 		});
+
+		it('api Counts in +1 increments', function(done) {
+			request(server.app)
+				.get('/api/counter', function(res) {
+					let testingValue = res.body.counterValue;
+					console.log(testingValue);
+					request(server.app)
+						.get('/api/counter')
+						.expect({ counterValue: testingValue + 1 }, done);
+				})
+				.end(done);
+		});
+	});
+
+	it('Should close the server', function(done) {
+		server.closeServer();
+		request(server.app)
+			.get('/api')
+			.expect(404)
+			.end(function(err, res) {
+				if (err) return done(err);
+				done();
+			});
 	});
 });
